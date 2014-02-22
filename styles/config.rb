b5fccf8eeaa3f72940ce01da68227fab6ad9a1f3
Mo Henriquez <mhenriquez@Mos-MacBook-Air.local>
2014-02-22 00:06:37 -0500

    Updates and Fixes
    Fixes to last commit.

--- styles/config.rb
+++ styles/config.rb
@@ -9,10 +9,12 @@ javascripts_dir = "../scripts"
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 output_style = :expanded
+
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
+
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = false
+line_comments = true
 
 # Have Growl? http://growl.info
 # 1. run `gem install compass-growl`
